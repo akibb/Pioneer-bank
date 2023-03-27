9a6b060b8cda82f49ae64085662dec4584a47adb
akibb <akibctg123@gmail.com>
2023-03-27 15:11:25 +0600

    new

--- Script.js
+++ Script.js
@@ -1,33 +1,57 @@
 
 
 // Login Button Event Handler
-const loginBtn=document.getElementById("login");
-loginBtn.addEventListener("click",function(){
-    const loginArea =document.getElementById("login-area")
-    loginArea.style.display="none";
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener("click", function () {
+    const loginArea = document.getElementById("login-area")
+    loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
-    transactionArea.style.display ="block";
+    transactionArea.style.display = "block";
 })
 
 // Deposit Button Event Handler
 
 const depositBtn = document.getElementById("addDeposit");
-depositBtn.addEventListener("click",function(){
-    const depositAmount = document.getElementById("depositAmount").value;
-    const depositNumber = parseFloat(depositAmount);
+depositBtn.addEventListener("click", function () {
+    // const depositAmount = document.getElementById("depositAmount").value;
+    // const depositNumber = parseFloat(depositAmount);
+    const depositNumber = getInputNumber("depositAmount");
 
-     const currentDeposit = document.getElementById("currentDeposit").innerText;
-     const currentDepositNumber= parseFloat(currentDeposit);
-     const totalDeposit = depositNumber + currentDepositNumber;
+    //  const currentDeposit = document.getElementById("currentDeposit").innerText;
+    //  const currentDepositNumber= parseFloat(currentDeposit);
+    //  const totalDeposit = depositNumber + currentDepositNumber;
 
-     document.getElementById("currentDeposit").innerText = totalDeposit;
+    //  document.getElementById("currentDeposit").innerText = totalDeposit;
 
-     const currentBalance = document.getElementById("currentBalance").innerText;
-     const currentBalanceNumber = parseFloat(currentBalance);
-     const totalBalance = depositNumber + currentBalanceNumber ;
+    //  Balance Button
+    //  const currentBalance = document.getElementById("currentBalance").innerText;
+    //  const currentBalanceNumber = parseFloat(currentBalance);
+    //  const totalBalance = depositNumber + currentBalanceNumber ;
 
-     document.getElementById ("currentBalance").innerText = totalBalance;
+    //  document.getElementById ("currentBalance").innerText = totalBalance;
 
+    updateSpanText("currentDeposit", depositNumber);
+    updateSpanText("currentBalance", depositNumber);
+    document.getElementById("depositAmount").value = "";
+})
 
-     document.getElementById("depositAmount").value = "";
-    })
+// Withdraw Button Handeler
+
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click", function(){
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    console.log(withdrawNumber);
+})
+
+function getInputNumber(id){
+    const withdrawAmount= document.getElementById(id).value;
+    const withdrawNumber = parseFloat(withdrawAmount);
+    return withdrawNumber;
+}
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+
+    document.getElementById(id).innerText = totalAmount;
+}
